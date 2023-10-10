f7484129ed4cb2c996fa6759d125bd90335e61e1
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-10-10 19:53:31 +0530

    inject remote data source. Closes #13

--- app/src/main/java/com/illuminateskills/foodapp/RemoteDataSource.kt
+++ app/src/main/java/com/illuminateskills/foodapp/RemoteDataSource.kt
@@ -1,4 +1,16 @@
 package com.illuminateskills.foodapp
 
-class RemoteDataSource {
+import com.illuminateskills.foodapp.interfaces.FoodRecipesApi
+import com.illuminateskills.foodapp.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    fun getRecipes(queries: Map<String, String>): Response<FoodRecipe>{
+        return foodRecipesApi.getRecipes(queries)
+    }
+
 }
