c5af74665e0b2c202f251808d11048dbc441d4c7
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-10-10 21:32:55 +0530

    created a class to Manage Recyclerview Adapter. Closes #17

---
+++ app/src/main/java/com/illuminateskills/foodapp/adapters/RecipesAdapter.kt
@@ -0,0 +1,54 @@
+package com.illuminateskills.foodapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.illuminateskills.foodapp.databinding.RecipesRowLayoutBinding
+import com.illuminateskills.foodapp.models.FoodRecipe
+import com.illuminateskills.foodapp.models.Result
+
+class RecipesAdapter: RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+private var recipe = emptyList<Result>()
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding):
+        RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(result: Result){
+                binding.result = result
+                binding.executePendingBindings()
+            }
+
+        companion object{
+            fun from(parent: ViewGroup): MyViewHolder{
+
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesRowLayoutBinding.inflate(
+                    layoutInflater, parent, false
+                )
+                return MyViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        return MyViewHolder.from(parent)
+    }
+
+    override fun getItemCount(): Int {
+
+        return  recipe.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val currentResult = recipe[position]
+        holder.bind(currentResult)
+    }
+
+    fun setData(newData: FoodRecipe){
+
+        recipe = newData.results
+        notifyDataSetChanged()
+    }
+}
