473ca81e553114607e081df6470f9d7c08c971b3
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-10-10 20:07:09 +0530

    created a class to handle Network Results. Closes #15

---
+++ app/src/main/java/com/illuminateskills/foodapp/util/NetworkResult.kt
@@ -0,0 +1,13 @@
+package com.illuminateskills.foodapp.util
+
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+){
+
+  class Success<T>(data: T): NetworkResult<T>(data)
+  class Error<T>(message: String? = null, data: T? = null):
+      NetworkResult<T>(data,message)
+  class Loading<T>: NetworkResult<T>()
+
+}
