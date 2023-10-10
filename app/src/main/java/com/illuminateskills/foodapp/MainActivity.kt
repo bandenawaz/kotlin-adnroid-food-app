64e9e1a293fd19d53fcc263cd52355fd061e3682
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-10-10 07:21:48 +0530

    code for handling navigation between fragments. Closes #6

--- app/src/main/java/com/illuminateskills/foodapp/MainActivity.kt
+++ app/src/main/java/com/illuminateskills/foodapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.illuminateskills.foodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    // ViewBindingDelegate
+    //private val binding: MainActivity by viewBinding(MainActivity::inflate)
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById(R.id.bottomNavigationView) as BottomNavigationView
+        //navController = findNavController(R.id.navHostFragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.recipiesFragment, R.id.favouriteRecipiesFragment,
+                R.id.foodJokeFragment)
+        )
+
+       bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
